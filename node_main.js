adc734fa5b97663a694e6e48ba0130a90313117b
Felix E. Klee <felix.klee@inka.de>
2014-11-23 20:47:34 +0000

    Fix: Inefficient memory use

--- node_main.js
+++ node_main.js
@@ -5,18 +5,79 @@
 
 'use strict';
 
+var square, initAB, createSa, createSb, createSc, createSd, createK,
+    initAB, solve = require('tridiagonal-solve');
+
+square = function (x) {
+    return x * x;
+};
+
+createSa = function (px, n) {
+    var sa = [], i;
+
+    for (i = 1; i <= n; i += 1) {
+        sa[i - 1] = 1 / (px[i] - px[i - 1]);
+    }
+
+    return sa;
+};
+
+createSb = function (px, n) {
+    var sb = [], i;
+
+    sb[0] = 2 / (px[1] - px[0]);
+    sb[n] = 2 / (px[n] - px[n - 1]);
+
+    for (i = 1; i < n; i += 1) {
+        sb[i] = 2 * (1 / (px[i] - px[i - 1]) + 1 / (px[i + 1] - px[i]));
+    }
+
+    return sb;
+};
+
+createSc = function (px, n) {
+    var sc = [], i;
+
+    for (i = 0; i < n; i += 1) {
+        sc[i] = 1 / (px[i + 1] - px[i]);
+    }
+
+    return sc;
+};
+
+createSd = function (px, py, n) {
+    var sd = [], i;
+
+    sd[0] = 3 * (py[1] - py[0]) / square(px[1] - px[0]);
+    sd[n] = 3 * (py[n] - py[n - 1]) / square(px[n] - px[n - 1]);
+    for (i = 1; i < n; i += 1) {
+        sd[i] = 3 * ((py[i] - py[i - 1]) / square(px[i] - px[i - 1]) +
+                     (py[i + 1] - py[i]) / square(px[i + 1] - px[i]));
+    }
+
+    return sd;
+};
+
+createK = function (px, py, n) {
+    return solve(createSa(px, n),
+                 createSb(px, n),
+                 createSc(px, n),
+                 createSd(px, py, n));
+};
+
+initAB = function (a, b, px, py, n) {
+    var i, k = createK(px, py, n);
+
+    for (i = 1; i <= n; i += 1) {
+        a[i] = k[i - 1] * (px[i] - px[i - 1]) - (py[i] - py[i - 1]);
+        b[i] = -k[i] * (px[i] - px[i - 1]) + (py[i] - py[i - 1]);
+    }
+};
+
 module.exports = function (points) {
     var n = points.length - 1,
         px,
         py,
-        solve = require('tridiagonal-solve'),
-        square,
-        initAB,
-        createSa,
-        createSb,
-        createSc,
-        createSd,
-        createK,
         q,
         a = [],
         b = [];
@@ -33,75 +94,9 @@ module.exports = function (points) {
         };
     }
 
-    square = function (x) {
-        return x * x;
-    };
-
-    createSa = function () {
-        var sa = [], i;
-
-        for (i = 1; i <= n; i += 1) {
-            sa[i - 1] = 1 / (px[i] - px[i - 1]);
-        }
-
-        return sa;
-    };
-
-    createSb = function () {
-        var sb = [], i;
-
-        sb[0] = 2 / (px[1] - px[0]);
-        sb[n] = 2 / (px[n] - px[n - 1]);
-
-        for (i = 1; i < n; i += 1) {
-            sb[i] = 2 * (1 / (px[i] - px[i - 1]) + 1 / (px[i + 1] - px[i]));
-        }
-
-        return sb;
-    };
-
-    createSc = function () {
-        var sc = [], i;
-
-        for (i = 0; i < n; i += 1) {
-            sc[i] = 1 / (px[i + 1] - px[i]);
-        }
-
-        return sc;
-    };
-
-    createSd = function () {
-        var sd = [], i;
-
-        sd[0] = 3 * (py[1] - py[0]) / square(px[1] - px[0]);
-        sd[n] = 3 * (py[n] - py[n - 1]) / square(px[n] - px[n - 1]);
-        for (i = 1; i < n; i += 1) {
-            sd[i] = 3 * ((py[i] - py[i - 1]) / square(px[i] - px[i - 1]) +
-                         (py[i + 1] - py[i]) / square(px[i + 1] - px[i]));
-        }
-
-        return sd;
-    };
-
-    createK = function () {
-        return solve(createSa(), createSb(), createSc(), createSd());
-    };
-
-    initAB = function () {
-        var i, k = createK();
-
-        for (i = 1; i <= n; i += 1) {
-            a[i] = k[i - 1] * (px[i] - px[i - 1]) - (py[i] - py[i - 1]);
-            b[i] = -k[i] * (px[i] - px[i - 1]) + (py[i] - py[i - 1]);
-        }
-    };
-
     points = points.sort(function (a, b) {
         return a[0] - b[0];
     });
-
-    px = points.map(function (point) { return point[0]; });
-    py = points.map(function (point) { return point[1]; });
 
     // Interpolating polynomial for px[i-1] <= x <= px[i] where 1 <= i <= n.
     q = function (i, x) {
@@ -112,7 +107,10 @@ module.exports = function (points) {
                 t * (1 - t) * (a[i] * (1 - t) + b[i] * t));
     };
 
-    initAB();
+    px = points.map(function (point) { return point[0]; });
+    py = points.map(function (point) { return point[1]; });
+
+    initAB(a, b, px, py, n);
 
     return function (x) {
         var i;
